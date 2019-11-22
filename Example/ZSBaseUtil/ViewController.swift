6a04c2d28da1729f524a22e5a2681bdbbafa78c8
zhangsen093725 <376019018@qq.com>
2019-11-22 14:04:51 +0800

    ZSWebView 新增代理回调

--- Example/ZSBaseUtil/ViewController.swift
+++ Example/ZSBaseUtil/ViewController.swift
@@ -20,16 +20,31 @@ class ViewController: UIViewController {
         return phoneField
     }()
     
+    lazy var safariView: ZSWebView = {
+        
+        let safariView = ZSWebView()
+        view.addSubview(safariView)
+        return safariView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         phoneField.frame = CGRect(x: 20, y: 100, width: 200, height: 30)
         // Do any additional setup after loading the view, typically from a nib.
+        safariView.load(url: "https://www.baidu.com".zs_url!)
+        
+        navigationController?.navigationBar.isTranslucent = false
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        safariView.frame = view.bounds
     }
     
 }
--- ZSBaseUtil/Classes/WebView/ZSWebView.swift
+++ ZSBaseUtil/Classes/WebView/ZSWebView.swift
@@ -11,24 +11,59 @@ import JavaScriptCore
 
 @objc public protocol ZSWebViewDelegate {
     
+    /// html 是否允许跳转 link
+    /// - Parameter webView: 当前的web
+    /// - Parameter navigationAction: 跳转事件
     @objc optional func zs_webView(_ webView: WKWebView, isDecidePolicy navigationAction: WKNavigationAction) -> Bool
     
+    /// html open new window, 返回 nil 则不跳转
+    /// - Parameter webView: 当前的web
+    /// - Parameter configuration: 当前的web的 configuration
+    /// - Parameter navigationAction: 跳转事件
+    /// - Parameter windowFeatures: 当前的web的 windowFeatures
+    @objc optional func zs_webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView?
+    
+    /// html alert
+    /// - Parameter webView: 当前的web
+    /// - Parameter message: 当前的 web alert 的 message
     @objc optional func zs_webView(_ webView: WKWebView, alert message: String)
     
+    /// html 加载完毕
+    /// - Parameter webView: 当前的web
     @objc optional func zs_webViewDidLoad(_ webView: WKWebView)
     
+    /// html 加载失败
+    /// - Parameter webView: 当前的web
+    /// - Parameter error: 错误信息
     @objc optional func zs_webView(_ webView: WKWebView, loadFail error: Error)
     
+    /// html 开始加载
+    /// - Parameter webView: 当前的web
     @objc optional func zs_webViewBeginLoad(_ webView: WKWebView)
     
+    /// webView 滑动回调
+    /// - Parameter scrollView: 当前的 web scrollView
     @objc optional func zs_webViewDidScroll(_ scrollView: UIScrollView)
     
+    /// html 加载进度
+    /// - Parameter webView: 当前的 web
+    /// - Parameter progress: 进度
     @objc optional func zs_webView(_ webView: WKWebView, loadFor progress : Float)
     
+    /// html title
+    /// - Parameter webView: 当前的 web
+    /// - Parameter title: 标题
     @objc optional func zs_webView(_ webView: WKWebView, title: String)
+    
+    /// html 当前页面是否Root路径
+    /// - Parameter webView: 当前的 web
+    /// - Parameter isRootWeb: 是否Root路径
+    @objc optional func zs_webView(_ webView: WKWebView, isRootWeb: Bool)
 }
 
 @objcMembers public class ZSWebView: UIView, UIScrollViewDelegate, UIGestureRecognizerDelegate, WKUIDelegate, WKNavigationDelegate {
+    
+    private var isCanBack: Bool = false
     
     @objc private lazy var webView: WKWebView = {
         
@@ -36,25 +71,32 @@ import JavaScriptCore
         configuration.userContentController = WKUserContentController()
         configuration.selectionGranularity = .character
         configuration.allowsInlineMediaPlayback = true
+        configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
         
         let webView = WKWebView(frame: .zero, configuration: configuration)
         webView.scrollView.delegate = self
         webView.navigationDelegate = self
         webView.uiDelegate = self
+        webView.isUserInteractionEnabled = false
         
         webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
         webView.addObserver(self, forKeyPath: "title", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: "canGoBack", options: .new, context: nil)
         
-        let longPress = UILongPressGestureRecognizer(target: self, action: nil)
-        longPress.minimumPressDuration = 0.2
-        longPress.delegate = self
-        
-        webView.addGestureRecognizer(longPress)
+//        let longPress = UILongPressGestureRecognizer(target: self, action: nil)
+//        longPress.minimumPressDuration = 0.2
+//        longPress.delegate = self
+//
+//        webView.addGestureRecognizer(longPress)
         webView.scrollView.decelerationRate = .normal
         
         if #available(iOS 11.0, *) {
             webView.scrollView.contentInsetAdjustmentBehavior = .never
         }
+        
+        let gesture = UIPanGestureRecognizer.init(target: target, action: #selector(backGestureRecognizer(_:)))
+        gesture.delegate = self
+        addGestureRecognizer(gesture)
         
         addSubview(webView)
         return webView
@@ -76,6 +118,7 @@ import JavaScriptCore
     
     public var isAllowZoom: Bool = false
     
+    public var isBackGestureRecognizerEnable: Bool = true
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -126,6 +169,8 @@ import JavaScriptCore
     // TODO: observe
     public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
+        guard object as? WKWebView == webView else { return }
+        
         if keyPath == "title" {
             delegate?.zs_webView?(webView, title: webView.title ?? "unknow")
             return
@@ -136,14 +181,45 @@ import JavaScriptCore
             delegate?.zs_webView?(webView, loadFor: progress.floatValue)
             return
         }
+        
+        if keyPath == "canGoBack" {
+            isCanBack = change?[.newKey] as! Bool
+        }
+    }
+    
+    @objc func backGestureRecognizer(_ recognizer: UIPanGestureRecognizer) {
+        
+        guard isCanBack else { return }
+        
+        guard recognizer.state == .ended else { return }
+        
+        let point = recognizer.location(in: recognizer.view)
+        
+        let translation = recognizer.translation(in: recognizer.view)
+        
+        let absX = fabsf(Float(translation.x))
+        let absY = fabsf(Float(translation.y))
+        
+        if (absX > absY ) {
+            
+            print(translation)
+            
+            if (translation.x < 0) {
+                //向左滑动
+                
+            } else if (point.x < KDevice.width * 0.3) {
+                //向右滑动
+                webView.goBack()
+            }
+        }
     }
     
     // TODO: UIGestureRecognizerDelegate
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         next?.touchesBegan([touch], with: nil)
-        return false
+        return isBackGestureRecognizerEnable
     }
-
+    
     // TODO: UIScrollViewDelegate
     public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return isAllowZoom ? webView.scrollView.subviews.first : nil
@@ -170,7 +246,9 @@ import JavaScriptCore
         delegate?.zs_webView?(webView, loadFail: error)
     }
     
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    public func webView(_ webView: WKWebView,
+                        decidePolicyFor navigationAction: WKNavigationAction,
+                        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
         if delegate?.zs_webView?(webView, isDecidePolicy: navigationAction) ?? true {
             decisionHandler(.allow)
@@ -180,6 +258,14 @@ import JavaScriptCore
         decisionHandler(.cancel)
     }
     
+    public func webView(_ webView: WKWebView,
+                        createWebViewWith configuration: WKWebViewConfiguration,
+                        for navigationAction: WKNavigationAction,
+                        windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        return delegate?.zs_webView?(webView, createWebViewWith: configuration, for: navigationAction, windowFeatures: windowFeatures)
+    }
+    
     // TODO: WKUIDelegate
     public func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
         // Alert
@@ -187,9 +273,9 @@ import JavaScriptCore
         completionHandler()
     }
     
-    public func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+    public func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
         // Alert
-        delegate?.zs_webView?(webView, alert: message)
-        completionHandler(false)
+        delegate?.zs_webView?(webView, alert: prompt)
+        completionHandler(nil)
     }
 }
