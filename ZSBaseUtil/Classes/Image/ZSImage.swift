998c9676cb1a07c91aba11b8012ae422ba32d92b
zhangsen093725 <376019018@qq.com>
2020-02-07 22:02:04 +0800

    修改zs_fixOrientation为zs_fixOrientationImage

--- ZSBaseUtil/Classes/Image/ZSImage.swift
+++ ZSBaseUtil/Classes/Image/ZSImage.swift
@@ -9,48 +9,7 @@ import UIKit
 
 public extension UIImage {
     
-    func zs_scaled(_ newSize: CGSize) -> UIImage {
-        
-        guard newSize != .zero else { return self }
-        
-        let clipW = size.width >= newSize.width ? newSize.width : size.width
-        let clipH = size.height >= newSize.height ? newSize.height : size.height
-        
-        guard let sourceImageRef = cgImage else { return self }
-        
-        guard let newImageRef = sourceImageRef.cropping(to: CGRect(x: (size.width - clipW) * 0.5, y: (size.height - clipH) * 0.5, width: clipW, height: clipH)) else { return self }
-        
-        return UIImage(cgImage: newImageRef)
-    }
-    
-    func zs_corner(radius: CGFloat) -> UIImage {
-        
-        let width = size.width * scale
-        let height = size.height * scale
-        let rect = CGRect(x: 0, y: 0, width: width, height: height)
-        
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 1.0)
-        UIBezierPath(roundedRect: rect, cornerRadius: radius).addClip()
-        draw(in: rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image ?? self
-    }
-    
-    func zs_mask(image: UIImage) -> UIImage {
-        
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
-        draw(in: rect)
-        image.draw(in: rect, blendMode: .destinationIn, alpha: 1)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image ?? self
-    }
-    
-    func zs_fixOrientation() -> UIImage {
+    var zs_fixOrientationImage: UIImage {
 
         guard imageOrientation != .up else { return self }
 
@@ -92,6 +51,47 @@ public extension UIImage {
         let img = UIImage(cgImage: cgimg)
         
         return img
+    }
+    
+    func zs_scaled(_ newSize: CGSize) -> UIImage {
+        
+        guard newSize != .zero else { return self }
+        
+        let clipW = size.width >= newSize.width ? newSize.width : size.width
+        let clipH = size.height >= newSize.height ? newSize.height : size.height
+        
+        guard let sourceImageRef = cgImage else { return self }
+        
+        guard let newImageRef = sourceImageRef.cropping(to: CGRect(x: (size.width - clipW) * 0.5, y: (size.height - clipH) * 0.5, width: clipW, height: clipH)) else { return self }
+        
+        return UIImage(cgImage: newImageRef)
+    }
+    
+    func zs_corner(radius: CGFloat) -> UIImage {
+        
+        let width = size.width * scale
+        let height = size.height * scale
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 1.0)
+        UIBezierPath(roundedRect: rect, cornerRadius: radius).addClip()
+        draw(in: rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? self
+    }
+    
+    func zs_mask(image: UIImage) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        draw(in: rect)
+        image.draw(in: rect, blendMode: .destinationIn, alpha: 1)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? self
     }
     
     class func zs_image(color: UIColor) -> UIImage? {
